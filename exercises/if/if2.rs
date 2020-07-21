a7b3ec0cc60dc6cbb910235f1223657046b002c8
Ian MacGuigan <ian.macguigan@gmail.com>
2020-07-21 15:06:40 -0400

    if2

--- exercises/if/if2.rs
+++ exercises/if/if2.rs
@@ -3,14 +3,12 @@
 // Step 1: Make me compile!
 // Step 2: Get the bar_for_fuzz and default_to_baz tests passing!
 // Execute the command `rustlings hint if2` if you want a hint :)
-
-// I AM NOT DONE
-
 pub fn fizz_if_foo(fizzish: &str) -> &str {
-    if fizzish == "fizz" {
-        "foo"
-    } else {
-        1
+    // not an if, but I think it's better
+    match fizzish {
+	"fizz" => "foo",
+	"fuzz" => "bar",
+	_      => "baz",
     }
 }
 
