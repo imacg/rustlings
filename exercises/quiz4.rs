822c830fb7a868eacabaf76e7dec0252b174ddb4
Ian MacGuigan <ian.macguigan@gmail.com>
2020-07-22 23:15:46 -0400

    quiz4

--- exercises/quiz4.rs
+++ exercises/quiz4.rs
@@ -5,7 +5,11 @@
 
 // Write a macro that passes the quiz! No hints this time, you can do it!
 
-// I AM NOT DONE
+macro_rules! my_macro {
+    ($v:tt) => {
+	concat!("Hello ", $v);
+    };
+}
 
 #[cfg(test)]
 mod tests {
