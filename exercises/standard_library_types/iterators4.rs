315efee1af54c305fc7f59d8df36cd9e9c74ba4b
Ian MacGuigan <ian.macguigan@gmail.com>
2020-07-26 00:36:48 -0400

    standard_library_types

--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -1,6 +1,6 @@
 // iterators4.rs
 
-// I AM NOT DONE
+use std::iter::successors;
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -12,6 +12,7 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    successors(Some(1), |&n| if n >= num { None } else { Some(n+1) }).product()
 }
 
 #[cfg(test)]
